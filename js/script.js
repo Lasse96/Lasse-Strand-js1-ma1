cc9a23574a03f98966e01c8365e71477c6ee667b
Lasse96 <91594348+Lasse96@users.noreply.github.com>
2022-01-28 13:27:47 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,3 +12,55 @@ const cats = [
     }
 ];
 
+// Oppgave 1
+function cat(complain) {
+    return complain;
+};
+
+let result = cat("Meow!")
+console.log(result);
+
+// Oppgave 2
+const heading = document.querySelector("h3");
+heading.innerHTML = "Updated heading";
+
+// Oppgave 3
+heading.style.fontSize = "2em";
+
+// Oppgave 4
+heading.classList = "subheading";
+
+// Oppgave 5
+const paragraphs = document.querySelectorAll("p");
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].style.color = "blue";
+};
+
+// Oppgave 6
+const resultsContainer = document.querySelector("div.results");
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
+// Oppgave 7
+
+function listcats(list) {
+    let string = "";
+    for (let cat of list) {
+        string += console.log(`${cat.name} ${cat.age}`);
+    }
+    return string;
+};
+listcats(cats);
+
+// Oppgave 8
+function createCats(cats) {
+    let string2 = "";
+    for (const create of cats) {
+        string2 += `<div><h5>${create.name}</h5></div>`;
+        if (create.age) string2 += `<div><p>${create.age}</p></div>`;
+        else string2 += "Age unknown";
+    }
+    return string2;
+};
+const output = document.querySelector("div.cat-container");
+output.innerHTML = createCats(cats);
